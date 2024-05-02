ab2ed0818105f9f6955a1ddbb8bbd05ceda442fb
Bhargavi R <98413345+Bhargavi61@users.noreply.github.com>
2024-05-02 16:21:04 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,5 @@
-# Initialize a new GitHub repository if you don't have one
-git init
-git remote add origin <YOUR_GITHUB_REPO_URL>
 
-# Add files to the repository and commit
 git add index.html
 git commit -m "Deploy chatbot frontend"
 
-# Push to GitHub and enable GitHub Pages
 git push -u origin master
